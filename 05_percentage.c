3737d58f15c882b9484f451e1a49fbedb765bb96
Soham-Debnath <20debnathsoham04@gmail.com>
2025-01-06 01:05:31 +0530

    percentage

---
+++ 05_percentage.c
@@ -0,0 +1,14 @@
+
+// finding percentage marks of a student
+# include<stdio.h>
+int main(){
+    float maths=95;
+    float bengali=95;
+    float english=96;
+    float biology=83;
+    float physics=86;
+    // float chemistry=77
+
+    printf("total marks=%f\npercentage=%f %%",(maths+bengali+english+biology+physics),(maths+bengali+english+biology+physics)/5);
+    return 0;
+}
