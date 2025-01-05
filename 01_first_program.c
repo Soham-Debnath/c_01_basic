3dca3c7deb288ea47cb34c85db212ee26ae62a7a
Soham-Debnath <20debnathsoham04@gmail.com>
2025-01-06 01:00:25 +0530

    first

---
+++ 01_first_program.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // include the standard input output library
+int main(){
+    printf("Hello World \n");
+    printf("Hi my name is \nSoham"); // \n is line break
+    return 0;
+
+}
